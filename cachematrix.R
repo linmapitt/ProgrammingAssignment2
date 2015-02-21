f8f7ffff073a2ed3a1ca27b3e6a43769d3809125
linmapitt <linma_pitt@yahoo.com>
2015-02-21 18:10:55 -0500

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inv <- NULL ##assign NULL to inv as placeholder for future
+  
+  ## function set sets the value in a parent 
+  ## environment(makeCacheMatrix scope) that contains a variable called x
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
+  
+  ## function get returns variable x in makeCacheMatrix scope 
   get <- function() x
+  
+  ## function setinv sets the value in a parent 
+  ## environment(makeCacheMatrix scope) that contains a variable called inv
   setinv <- function(m_inv) inv <<- m_inv
+  
+  ## function getinv returns variable inv in makeCacheMatrix scope
   getinv <- function() inv
   list(set = set, get = get,
        setinv = setinv,
@@ -22,11 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## try to return cached inv  
   inv<- x$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
+  
+  ##if no cached inv, call solve function to calculate inv
+  ## and cache inv for future use
   matrix <- x$get()
   inv <- solve(matrix, ...)
   x$setinv(matrix)
